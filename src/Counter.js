81552add26ade5b2bc0c62372d1d68a55f79c602
chaipa <chaitanya.patel@flemingcollege.ca>
2020-08-04 12:03:02 -0400

    add action creator and constant

--- src/Counter.js
+++ src/Counter.js
@@ -1,52 +1,53 @@
 import React from "react"
-import {connect} from "react-redux"
+import { connect } from "react-redux"
+import { INCREMENT, DECREMENT, increment, decrement, reset } from "./actions"
 
 class Counter extends React.Component {
     // state = { count: 0 }; // remove this
-  
+
     increment = (props) => {
-        this.props.dispatch({type:"INCREMENT"})
-    };
-  
-    decrement = () => {
-      /*
-      // Also remove this
-      this.setState({
-        count: this.state.count - 1
-      });
-      */
-     this.props.dispatch({type:"DECREMENT"})
+        this.props.dispatch(increment())
     };
 
-    reset = ()=>{
-        this.props.dispatch({type:"RESET"})
+    decrement = () => {
+        /*
+        // Also remove this
+        this.setState({
+          count: this.state.count - 1
+        });
+        */
+        this.props.dispatch(decrement())
+    };
+
+    reset = () => {
+        this.props.dispatch(reset())
     }
-  
+
     render() {
-      return (
-        <div className="counter">
-          <h2>Counter</h2>
-          <div>
-            <button onClick={this.decrement}>-</button>
-            <span className="count">{
-              // Replace state:
-              //// this.state.count
-              // With props:
-              this.props.count
-            }</span>
-            <button onClick={this.increment}>+</button>
-            <button onClick={this.reset}>RESET</button>
-          </div>
-        </div>
-      );
+        return (
+            <div className="counter">
+                <h2>Counter</h2>
+                <div>
+                    <button onClick={this.decrement}>-</button>
+                    <span className="count">{
+                        // Replace state:
+                        //// this.state.count
+                        // With props:
+                        this.props.count
+                    }</span>
+                    <button onClick={this.increment}>+</button>
+                    <button onClick={this.reset}>RESET</button>
+                </div>
+            </div>
+        );
     }
-  }
+}
 
 function mapStateToProps(state) {
     return {
-      count: state.count    
+        count: state.count
     };
-  }
+}
 
 
 
